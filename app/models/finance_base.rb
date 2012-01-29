f125575d771014a6aae167c9d58353754cf90765
Lennart Prelle <me@lennartprelle.de>
2012-01-29 23:30:33 +0100

    added base model for finances

---
+++ app/models/finance_base.rb
@@ -0,0 +1,13 @@
+class FinanceBase < ActiveRecord::Base
+
+  establish_connection(
+{ :adapter  => "mysql2",
+  :host     => "localhost",
+  :username => Diaspora::Application.config.database_configuration[Rails.env]["username"],
+  :password => Diaspora::Application.config.database_configuration[Rails.env]["password"],
+  :database => "finances" }
+  )
+  
+  self.abstract_class = true
+  
+end
