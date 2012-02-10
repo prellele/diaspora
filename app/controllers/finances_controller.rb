1626605f060ec3fe1283c997fd7bb9afe2bcc187
Lennart Prelle <me@lennartprelle.de>
2012-02-10 16:54:43 +0100

    finances rework part 1

--- app/controllers/finances_controller.rb
+++ app/controllers/finances_controller.rb
@@ -2,11 +2,14 @@ class FinancesController < ApplicationController
   before_filter :authenticate_user!
   
   def index
+    onetime = [1,3,4,5] 
     @expenses = Expense.all
-    @onetime_revenues = Revenue.where(:type_id => [1,3,4,5])
-    @monthly_revenues = Revenue.where(:type_id => 2)
+    @onetimee_revenues = Revenue.where(:type_id => [1,3,4,5])
+    @monthlyy_revenues = Revenue.where(:type_id => 2)
     @sum_revenues = Revenue.sum("value")
     @sum_expenses = Expense.sum("value")
+    @onetime_revenues =  Revenue.where('t.id in (?)', onetime).joins("left join `types` t ON t.id = type_id").select("revenues.description,revenues.date,revenues.value,t.name")
+    @monthly_revenues =  Revenue.where('t.id not in (?)', onetime).joins("left join `types` t ON t.id = type_id").select("revenues.description,revenues.date,revenues.value,t.name")
   end
   
 end
