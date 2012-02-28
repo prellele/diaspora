05826cd7693b94be86ce88f64f2db934fd87131b
Lennart Prelle <me@lennartprelle.de>
2012-02-28 18:41:08 +0100

    provide finances balance from being cached

--- app/helpers/finances_helper.rb
+++ app/helpers/finances_helper.rb
@@ -1,7 +1,7 @@
 module FinancesHelper
 
   def effective_expenses
-    @@effective_expenses ||= Revenue.sum("value") - Expense.sum("value")
+    Revenue.sum("value") - Expense.sum("value")
   end
 
 end
