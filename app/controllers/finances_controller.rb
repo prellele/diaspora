7d5e493dd89f49d290b5a1f0e80f924e32541c53
Lennart Prelle <me@lennartprelle.de>
2012-04-13 15:06:18 +0200

    update expenses table on finances page

--- app/controllers/finances_controller.rb
+++ app/controllers/finances_controller.rb
@@ -7,7 +7,7 @@ class FinancesController < ApplicationController
     @monthlyy_revenues = Revenue.where(:type_id => 2)
     @sum_revenues = Revenue.sum("value")
     @sum_expenses = Expense.sum("value")
-    @expenses = Expense.joins("left join `types` t ON t.id = type_id").select("expenses.description,expenses.date,t.name,COUNT(description) as anzahl,sum(expenses.value) as value").group('expenses.description')
+    @expenses = Expense.joins("left join `types` t ON t.id = type_id").select("expenses.description,min(expenses.date) as mindate,max(expenses.date) as maxdate,t.name,value,sum(expenses.value) as totalvalue").group('expenses.value, expenses.description')
     @onetime_revenues =  Revenue.where('t.id in (?)', onetime).joins("left join `types` t ON t.id = type_id").select("revenues.description,revenues.date,revenues.value,t.name")
     @monthly_revenues =  Revenue.where('t.id not in (?)', onetime).joins("left join `types` t ON t.id = type_id").select("revenues.description,revenues.date,sum(revenues.value) as value,t.name,COUNT(spender) as anzahl").group('revenues.spender')
     @total= @sum_expenses - @sum_revenues
