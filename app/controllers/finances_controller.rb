9546e635d3fcb9a61289ad1e6712b0cefef4ee40
Lennart Prelle <me@lennartprelle.de>
2012-07-11 18:28:35 +0200

    some modifications for finances data

--- app/controllers/finances_controller.rb
+++ app/controllers/finances_controller.rb
@@ -7,10 +7,10 @@ class FinancesController < ApplicationController
     @monthlyy_revenues = Revenue.where(:type_id => 2)
     @sum_revenues = Revenue.sum("value")
     @sum_expenses = Expense.sum("value")
-    @expenses = Expense.joins("left join `types` t ON t.id = type_id").select("expenses.description,min(expenses.date) as mindate,max(expenses.date) as maxdate,t.name,value,sum(expenses.value) as totalvalue, COUNT(value) as anzahl").group('expenses.value, expenses.description')
+    @expenses = Expense.joins("left join `types` t ON t.id = type_id").select("date, expenses.description,min(expenses.date) as mindate,max(expenses.date) as maxdate,t.name,value,till, COUNT(value) as anzahl").group('expenses.value, expenses.description')
     @onetime_revenues =  Revenue.where('t.id in (?)', onetime).joins("left join `types` t ON t.id = type_id").select("revenues.description,revenues.date,revenues.value,t.name")
-    @monthly_revenues =  Revenue.where('t.id not in (?)', onetime).joins("left join `types` t ON t.id = type_id").select("revenues.description,max(revenues.date) as maxdate,min(revenues.date) as mindate,value,sum(revenues.value) as totalvalue,t.name,COUNT(spender) as anzahl").group('revenues.spender')
-    @total= @sum_expenses - @sum_revenues
+    @monthly_revenues =  Revenue.where('t.id not in (?)', onetime).joins("left join `types` t ON t.id = type_id").select("revenues.description,till,date,value,t.name").group('revenues.spender')
+    @total = @sum_expenses - @sum_revenues
   end
 
 end
--- app/models/revenue.rb
+++ app/models/revenue.rb
@@ -1,4 +1,4 @@
-class Revenue < FinanceBase
+	class Revenue < FinanceBase
 
   belongs_to :type
 
@@ -7,7 +7,18 @@ class Revenue < FinanceBase
                   :date,
                   :value,
                   :spender,
-                  :type_id
+                  :type_id,
+                  :till
+  
+  def months
+    months = (till.nil?) ? Time.now.month-date.month : till.month-date.month
+  end
+  
+  def years
+    years = Time.now.year-date.year 
+    years += 1 if years <0 
+    years+1
+  end
 
 end
 
