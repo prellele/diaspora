965e6e45c586f9c66b1bd7b5deb05963b656d8ac
Lennart Prelle <me@lennartprelle.de>
2015-07-02 14:40:13 +0200

    read finance config from db config

--- app/models/finance_base.rb
+++ app/models/finance_base.rb
@@ -2,7 +2,7 @@ class FinanceBase < ActiveRecord::Base
 
   establish_connection(
 { :adapter  => "mysql2",
-  :host     => "localhost",
+  :host     => Diaspora::Application.config.database_configuration[Rails.env]["host"],
   :username => Diaspora::Application.config.database_configuration[Rails.env]["username"],
   :password => Diaspora::Application.config.database_configuration[Rails.env]["password"],
   :database => "finances" }
