5872a3f3195d23e894c046d4e023a0033e5fdb1e
Lennart Prelle <me@prellele.de>
2016-11-24 16:02:52 +0100

    remove datatables and structure from finances

--- app/helpers/finances_helper.rb
+++
@@ -1,8 +0,0 @@
-module FinancesHelper
-
-  def totalfinances
-  	return 0 unless Revenue.all.present? && Expense.all.present?
-    Revenue.all.map {|revenue| revenue.sumvalues}.inject(:+) - Expense.all.map {|expense| expense.sumvalues}.inject(:+) 
-  end
-
-end
